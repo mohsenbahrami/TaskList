56526de84e1289ebf4450e0ef9221e520d52bdd0
mohsenbahrami <mohsenbahrami_ah@yahoo.com>
2020-03-07 09:39:00 -0500

    make same change in notedetail

--- app/src/main/java/com/example/tasklist/NoteDetails.java
+++ app/src/main/java/com/example/tasklist/NoteDetails.java
@@ -4,55 +4,75 @@ public class NoteDetails {
 
         String title;
         String description;
+        String date;
         int shape;
         int status;
 
-        public NoteDetails(String title, String descrioption, int shape, int status ){
-            this.title = title;
-            this.description = descrioption;
-            this.shape = shape;
-            this.status = status;
-        }
+    public NoteDetails(String title, String description, String date, int shape, int status) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.shape = shape;
+        this.status = status;
+    }
 
-        public NoteDetails(String title, String descrioption, int status ){
-            this.title = title;
-            this.description = descrioption;
-            this.shape = 0;
-            this.status = status;
-        }
+    public NoteDetails() {
+    }
 
+    public NoteDetails(String title, String description, int shape, int status) {
+        this.title = title;
+        this.description = description;
+        this.shape = shape;
+        this.status = status;
+    }
 
-        public String getTitle() {
-            return title;
-        }
+    public NoteDetails(String title, String description, int shape) {
+        this.title = title;
+        this.description = description;
+        this.shape = shape;
+    }
 
-        public String getDescription() {
-            return description;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        public int getShape() {
-            return shape;
-        }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-        public int getStatus() {
-            return status;
-        }
+    public String getDescription() {
+        return description;
+    }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
+    public String getDate() {
+        return date;
+    }
 
-        public void setShape(int shape) {
-            this.shape = shape;
-        }
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-        public void setStatus(int status) {
-            this.status = status;
-        }
+    public int getShape() {
+        return shape;
+    }
 
+    public void setShape(int shape) {
+        this.shape = shape;
+    }
 
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
+
+
+
+
